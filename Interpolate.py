cc8170ab5c2121e0420b7ff5ba88a0260bef3f74
TalHayun <87673883+TalHayun@users.noreply.github.com>
2022-04-17 10:25:35 +0300

    Update Interpolate.py

--- Interpolate.py
+++ Interpolate.py
@@ -29,24 +29,7 @@ class Assignment1:
 
     def interpolate( self, f: callable, a: float, b: float, n: int ) -> callable:
         """
-        Interpolate the function f in the closed range [a,b] using at most n
-        points. Your main objective is minimizing the interpolation error.
-        Your secondary objective is minimizing the running time.
-        The assignment will be tested on variety of different functions with
-        large n values.
-
-        Interpolation error will be measured as the average absolute error at
-        2*n random points between a and b. See test_with_poly() below.
-
-        Note: It is forbidden to call f more than n times.
-
-        Note: This assignment can be solved trivially with running time O(n^2)
-        or it can be solved with running time of O(n) with some preprocessing.
-        **Accurate O(n) solutions will receive higher grades.**
-
-        Note: sometimes you can get very accurate solutions with only few points,
-        significantly less than n.
-
+        
         Parameters
         ----------
         f : callable. it is the given function
